272cac7dee37561548cbb4a7660ec8e9a349a304
Katharine Hammer <katharine.m.brown@gmail.com>
2021-06-27 12:11:19 -0600

    initial commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void StartGame() {
+        Debug.Log("Starting game!");
+    }
+
+    public void LoadSceneByName(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
